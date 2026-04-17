11443b41bd865d1f22771785e843256de4f99c8c
Akilesh6162 <138648093+Akilesh6162@users.noreply.github.com>
2026-04-17 16:12:55 +0530

    Login validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,67 +8,63 @@ import { RegiServService } from '../regi-serv.service';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
- 
-showSuccess:boolean=false;
-  store:any=[];
-  
-  values={
-    first:'',
-    last:'',
-    email:'',
-    password:'',
-    phno:''
+
+  showSuccess: boolean = false;
+  store: any = [];
+
+  values = {
+    first: '',
+    last: '',
+    email: '',
+    password: '',
+    phno: ''
   }
 
-  constructor(private f:FormBuilder,private b:RegiServService){}
-  
-    registration=this.f.group({
-      first:['',[Validators.required]],
-      last:['',[Validators.required]],
-      email:['',[Validators.required]],
-      password:['',[Validators.required]],
-      phno:['',[Validators.required]]
-    });
-  
-  
-  submit() {
-    if(this.registration.invalid){
-        this.registration.markAllAsTouched(); // show errors in UI
-    return; 
-        
-    }
+  constructor(private f: FormBuilder, private b: RegiServService) { }
 
-  const formData = this.registration.value;
-
-  this.b.post(formData).subscribe(() => {
-    this.getdata(); // refresh list
-    this.registration.reset(); // clear form
+  registration = this.f.group({
+    first: ['', [Validators.required]],
+    last: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    phno: ['', [Validators.required]]
   });
 
- this.showSuccess=true;
- 
-      setTimeout(()=>{
-      this.showSuccess=false;
+
+  submit() {
+    if (this.registration.invalid) {
+      this.registration.markAllAsTouched(); // show errors in UI
+      return;
+
+    }
+
+    const formData = this.registration.value;
+
+    this.b.post(formData).subscribe(() => {
+      this.getdata(); // refresh list
+      this.registration.reset(); // clear form
+    });
+
+    this.showSuccess = true;
+
+    setTimeout(() => {
+      this.showSuccess = false;
     }, 1500);
-}
+  }
 
-ngOnInit(){
-  this.getdata();
-}
-getdata(){
-  this.b.get().subscribe((data:any)=>{
-    this.store=data;
-  })
-}
-
-add(){
-  this.b.post(this.values).subscribe(()=>{
-    alert("the data added successfully");
+  ngOnInit() {
     this.getdata();
-  })
-}
+  }
+  getdata() {
+    this.b.get().subscribe((data: any) => {
+      this.store = data;
+    })
+  }
 
-
-
-
+  add() {
+    this.b.post(this.values).subscribe(() => {
+      alert("the data added successfully");
+      this.getdata();
+    })
+  }
 }
