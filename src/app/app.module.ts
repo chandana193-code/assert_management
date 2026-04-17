fb770e28000676254cc3179fd7ec084fdcaafea3
Karthiktech84 <mamidikarthik2004@gmail.com>
2026-04-17 14:49:32 +0530

    registration_required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< Updated upstream
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
-=======
 import { RegistrationComponent } from './registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> Stashed changes
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 import { UserDashboradComponent } from './users/user-dashborad/user-dashborad.component';
 import { MyAssetComponent } from './users/my-asset/my-asset.component';
@@ -22,26 +18,19 @@ import { RouterModule } from '@angular/router';
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< Updated upstream
     AdminDashboardComponent,
     AdminLayoutComponent, 
     MyAssetComponent,
-    UserDashboradComponent
-=======
+    UserDashboradComponent,
     RegistrationComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-<<<<<<< Updated upstream
     HttpClientModule,
     RouterModule   
-=======
-     HttpClientModule
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
