28fcb99f65541bdd234d485b79e2a028dc7c4f09
Delli Kumar V <dellikumar46@gmail.com>
2026-04-28 16:46:41 +0530

    created asset request for user

---
+++ src/app/users/request-asset/request-asset.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-asset',
+  templateUrl: './request-asset.component.html',
+  styleUrls: ['./request-asset.component.css']
+})
+export class RequestAssetComponent {
+
+}
