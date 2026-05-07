b0b3a26f16ec5c5d28b516262dc6aee3dbbe339a
dhina-0601 <dhinakaran0601@gmail.com>
2026-05-07 10:04:54 +0530

    changes in the add asset

--- src/app/asset.service.ts
+++ src/app/asset.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import {
+  HttpClient
+} from '@angular/common/http';
+
+import {
+  Observable
+} from 'rxjs';
 
 export interface Asset {
+
   id?: number;
+
+  assetId?: string;
+
   name: string;
+
   category: string;
+
   serialNumber: string;
+
   purchaseDate: string;
-  status: 'Available' | 'Assigned' | 'Repair';
+
+  warrantyDate?: string;
+
+  status: string;
+
   condition: string;
-  assignedUser: string;
+
+  cost?: number;
+
+  location?: string;
+
+  image?: string;
 }
 
 @Injectable({
@@ -18,27 +40,57 @@ export interface Asset {
 })
 export class AssetService {
 
-  private apiUrl = 'http://localhost:3000/asset';
+  private apiUrl =
+    'http://localhost:3000/asset';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
-  getAssets(): Observable<Asset[]> {
-    return this.http.get<Asset[]>(this.apiUrl);
+  getAssets():
+    Observable<Asset[]> {
+
+    return this.http.get<Asset[]>(
+      this.apiUrl
+    );
   }
 
-  createAsset(data: Asset): Observable<Asset> {
-    return this.http.post<Asset>(this.apiUrl, data);
+  createAsset(
+    data: FormData
+  ): Observable<any> {
+
+    return this.http.post<any>(
+      this.apiUrl,
+      data
+    );
   }
 
-  getAssetById(id: number): Observable<Asset> {
-    return this.http.get<Asset>(`${this.apiUrl}/${id}`);
+  getAssetById(
+    id: number
+  ): Observable<Asset> {
+
+    return this.http.get<Asset>(
+      `${this.apiUrl}/${id}`
+    );
   }
 
-  updateAsset(id: number, data: Partial<Asset>): Observable<Asset> {
-    return this.http.put<Asset>(`${this.apiUrl}/${id}`, data);
+  updateAsset(
+    id: number,
+    data: Partial<Asset>
+  ): Observable<Asset> {
+
+    return this.http.put<Asset>(
+      `${this.apiUrl}/${id}`,
+      data
+    );
   }
 
-  deleteAsset(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  deleteAsset(
+    id: number
+  ): Observable<void> {
+
+    return this.http.delete<void>(
+      `${this.apiUrl}/${id}`
+    );
   }
 }
